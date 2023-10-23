c6ea8a8a1a19601ef4d96815917083513943dee3
xShadowBlade <dnguy3n2010@gmail.com>
2023-10-23 16:24:21 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import path from "path";
 import { argv } from "process";
 import { interpretQuarkScript } from "./interpreter.js";
 
-// Get the command-line arguments
+// Get the command-line arguments and flags
 const [args, flags] = (() => {
     const args = [];
     const flags = {};
@@ -13,7 +13,7 @@ const [args, flags] = (() => {
         if (item.match(/(-|--)+/)) { // If it is a flag
             item = item.replace(/(-|--)+/g, "");
             const arr = item.split("=");
-            flags[arr[0]] = arr[1];
+            flags[arr[0]] = arr[1] ? arr[1] : true;
         } else {
             args.push(item);
         }
@@ -21,36 +21,45 @@ const [args, flags] = (() => {
     return [args, flags]
 })();
 
-// console.log(args, flags);
+if (flags["d"] || flags["debug"]) console.log(args, flags);
 
-const [, , fullCommand, filePath] = args;
+const [, , subCommand, ...argsSubcommand] = args;
 
-// Parse the full command into its parts
-const [command, subCommand] = fullCommand.split(" ");
+function displayHelp () {
+    console.log("Usage: quarkscript <command> <path/to/file.quarkscript>");
 
-// Check if a command and a file path are provided
-if (command) {
-    // Resolve the provided file path
-    const resolvedPath = path.resolve(filePath);
-
-    // Read the QuarkScript file
-    fs.readFile(resolvedPath, "utf8", (err, data) => {
-        if (err) {
-            console.error(`Error reading the file: ${err.message}`);
-        } else {
-            // Interpret or compile the QuarkScript code based on the subcommand
-            // eslint-disable-next-line no-lonely-if
-            if (command === "interpret" || command === "i") {
-                console.log(`Program ${resolvedPath} started.`);
-                interpretQuarkScript(data, flags);
-            } else {
-                console.error("Invalid subcommand. Use \"interpret\" or \"i\".");
-            }
-        }
-    });
-} else {
-    // Display usage information
-    console.log("Usage: quarkscript <command> <path/to/file.qs>");
     console.log("Commands:");
-    console.log("  quarkscript interpret|i <path/to/file.qs>   Interpret the QuarkScript code");
+    console.log("\tquarkscript help\tDisplays Help");
+    console.log("\tquarkscript interpret|i <path/to/file.quarkscript>\tInterpret the QuarkScript code");
+
+    console.log("Flags:");
+    console.log("\t-h || --help\tDisplays Help");
+    console.log("\t-d || --debug\tDisplays debug info");
+
+    process.exit(0);
+}
+if (!subCommand || flags["h"]|| flags["help"]) displayHelp();
+// Parse the full command into its parts
+
+switch (subCommand.toLowerCase()) {
+    case "i" || "interpret":
+        // Read the QuarkScript file
+        // Resolve the provided file path
+        const resolvedPath = path.resolve(argsSubcommand[0]);
+        fs.readFile(resolvedPath, "utf8", (err, data) => {
+            if (err) {
+                console.error(`Error reading the file: ${err.message}`);
+                process.exit(2);
+            }
+            // Interpret or compile the QuarkScript code based on the subcommand
+            console.log(`Program ${resolvedPath} started.`);
+            interpretQuarkScript(data, flags);
+        });
+    break;
+    case "":
+    case "help":
+    case "h":
+    default:
+        displayHelp();
+    break;
 }
